9dbb7173b99f7d27834a6b81c287e8ddffff740d
Juan Brusasca <juanbrusasca96@gmail.com>
2023-02-15 17:01:43 -0300

    desafioGraphQL

--- Clase44-GraphQL/dto/productoDTO.js
+++
@@ -1,9 +0,0 @@
-const productoSinInfo = ([producto]) => {
-	return {
-		title: producto.title,
-		price: producto.price,
-		thumbnail: producto.thumbnail,
-	};
-};
-
-export default { productoSinInfo };
